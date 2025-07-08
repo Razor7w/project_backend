e19c40ad68e452dc8f1d853eef6600b6b2375b77
Seba <seba.carroza@gmail.com>
2025-07-07 22:44:23 -0400

    update: arriendo

--- src/models/Arriendo.ts
+++ src/models/Arriendo.ts
@@ -1,16 +1,25 @@
-import { AllowNull, Column, DataType, Model, Table } from "sequelize-typescript";
-import Arriendo from "./Arriendo";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 @Table({ tableName: "arriendos" })
 class RegistroArriendo extends Model {
   @Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true })
   declare id: number;
 
-  @Column({ type: DataType.DATE(), allowNull: false, field: "fecha_inicio" })
+  @Column({
+    type: DataType.DATE,
+    allowNull: false,
+    field: "fecha_inicio",
+    defaultValue: DataType.NOW,
+  })
   declare fechaInicio: Date;
 
-  @Column({ type: DataType.DATE(), allowNull: true, field: "fecha_fin" })
-  declare fechaFin: Date;
+  @Column({
+    type: DataType.DATE,
+    allowNull: true,
+    field: "fecha_fin",
+    defaultValue: null,
+  })
+  declare fechaFin: Date | null;
 
   @Column({ type: DataType.STRING(6), allowNull: false, field: "patente_vehiculo" })
   declare patenteVehiculo: string;
@@ -19,7 +28,7 @@ class RegistroArriendo extends Model {
   declare tipoVehiculo: string;
 
   @Column({ type: DataType.STRING(10), allowNull: false, field: "rut_cliente" })
-  declare rutUsuario: string;
+  declare rutCliente: string;
 
   @Column({ type: DataType.STRING(50), allowNull: false, field: "nombre_cliente" })
   declare nombreCliente: string;
